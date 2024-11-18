f9469c81ec87b5f908c25018c197ef2e166e3794
Yan Kuzin <iankuzin@gmail.com>
2024-11-18 13:10:29 +0500

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    content: [
+        "./src/**/*.html", // All HTML files in the src folder and subfolders
+        "./src/**/*.js",   // Include JavaScript files if you're using Tailwind classes in JS
+    ],
+    theme: {
+        extend: {}, // Add custom themes or overrides here
+    },
+    plugins: [], // Add plugins like forms, typography, etc., if needed
+};
