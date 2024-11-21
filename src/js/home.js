7a587ce50090e73879216750acad3829d037790b
Yan Kuzin <iankuzin@gmail.com>
2024-11-21 12:30:44 +0500

    home page layout no css

---
+++ src/js/home.js
@@ -0,0 +1,45 @@
+// Collapsible element functionality
+var coll = document.getElementsByClassName("collapsible");
+for (let i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+// OpenMap
+document.addEventListener("DOMContentLoaded", function () {
+  const map = new ol.Map({
+    target: "map",
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.XYZ({
+          url: "https://{1-4}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png",
+          attributions: "© OpenStreetMap contributors & CARTO",
+        }),
+      }),
+    ],
+    view: new ol.View({
+      center: ol.proj.fromLonLat([-0.09, 51.505]),
+      zoom: 13,
+    }),
+  });
+});
+
+// JavaScript to handle navbar visibility on scroll
+document.addEventListener("DOMContentLoaded", function () {
+  const navbar = document.querySelector("#navbar");
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 50) {
+      navbar.classList.add("scrolled");
+    } else {
+      navbar.classList.remove("scrolled");
+    }
+  });
+});
