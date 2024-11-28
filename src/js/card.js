483d64d4193a2936b58a4c81859ef131c5ce81d2
Yan Kuzin <iankuzin@gmail.com>
2024-11-28 18:20:37 +0500

    added card page

---
+++ src/js/card.js
@@ -0,0 +1,18 @@
+// OpenMap
+document.addEventListener("DOMContentLoaded", function () {
+  const map = new ol.Map({
+    target: "map",
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.XYZ({
+          url: "https://{1-4}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png",
+          attributions: "© OpenStreetMap contributors & CARTO",
+        }),
+      }),
+    ],
+    view: new ol.View({
+      center: ol.proj.fromLonLat([98.2938, 8.0016]),
+      zoom: 15,
+    }),
+  });
+});
