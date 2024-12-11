754cd08962ec9c624c509db04cdf8398fba08ff8
Yan Kuzin <iankuzin@gmail.com>
2024-12-11 13:59:29 +0500

    adding faq to club page

--- src/js/club.js
+++ src/js/club.js
@@ -35,3 +35,19 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+//JS to entry criteria faq
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
